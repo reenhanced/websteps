c1b2fc0d7f05818ed3dc7e3c89c01d76c3b187b0
Nicholas Hance <nhance@reenhanced.com>
2013-01-03 21:23:28 -0500

    Rails 3.2.10 with bootstrap and mongoid.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Websteps::Application.config.secret_token = '173f7767fe35311fd77f01b5333630c385e7848e9402599262cf52aa3a7c9778135611ee4ccd2a78355421f11b3247b6202a0c756e93549a8162ee4aba476095'
